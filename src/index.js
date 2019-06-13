f86bd85fc7e10ebfcb12e31f286a021616ede438
chengshenglan <chengshenglan@dianwoba.com>
2019-06-13 20:33:34 +0800

    feat: multiple entries and cleanWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg'
+import printMe from './print'
 function component() {
   let element = document.createElement('div');
-
+  var btn = document.createElement('button');
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = '点击这里，然后查看 console！';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
 const config = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app2: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }, {
-      test: /\.(png|svg|jpg|gif)$/,
-      use: ['file-loader']
-    }]
-  }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    })
+  ]
 }
 
 module.exports = config;
